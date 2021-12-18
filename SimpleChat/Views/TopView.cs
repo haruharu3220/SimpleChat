5cdb4d7fb2c9105d19ffb9da1bf81ccdd5b6e4ef
haruharu3220 <87214943+haruharu3220@users.noreply.github.com>
2021-12-18 23:55:59 +0900

    コメント追加

--- SimpleChat/Views/TopView.cs
+++ SimpleChat/Views/TopView.cs
@@ -20,7 +20,8 @@ namespace SimpleChat.Views
 
         private void SendButton_Click(object sender, EventArgs e)
         {
-            //テスト
+            //テスト１
+            //テスト２
             this.ReceiveMessageTextBox.Text = this.SendMessageTextBox.Text;
         }
     }
