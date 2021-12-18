7cccc92d39b0c2b668aa1ac111a06aa8ba6897b1
yuu19 <yuu19@LAPTOP-PSRB67FM>
2021-12-18 22:31:43 +0900

    プロジェクト ファイルを追加します。

---
+++ SimpleChat/Program.cs
@@ -0,0 +1,20 @@
+﻿using SimpleChat.Views;
+using System;
+using System.Windows.Forms;
+
+namespace SimpleChat
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TopView());
+        }
+    }
+}
